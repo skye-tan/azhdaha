e8d17fec31101c49e7f02c4cee1e8fab520a7d65
skye-tan <amirpax32@gmail.com>
2025-08-16 22:07:20 +0330

    Analyze linear return type of functions.

--- analyzer/src/terminator.rs
+++ analyzer/src/terminator.rs
@@ -2,7 +2,7 @@
 
 use ariadne::{Fmt as _, Label, ReportBuilder};
 
-use repr::mir;
+use repr::mir::{self, RETURN_LOCAL};
 
 use crate::{
     DIAGNOSIS_REPORT_COLOR, LinearCtx,
@@ -18,42 +18,79 @@ impl LinearCtx<'_> {
         terminator: &Option<mir::Terminator>,
     ) -> anyhow::Result<bool> {
         match linear_local.status {
-            LinearStatus::Free | LinearStatus::Unknown => return Ok(false),
-            LinearStatus::Owner => (),
-        }
-
-        match terminator {
-            Some(terminator) => {
-                if !matches!(&terminator.kind, mir::TerminatorKind::Return) {
+            LinearStatus::Owner => {
+                if linear_local.local == RETURN_LOCAL {
                     return Ok(false);
                 }
 
-                report_builder.set_message("Memory leakage after return");
+                match terminator {
+                    Some(terminator) => {
+                        if !matches!(&terminator.kind, mir::TerminatorKind::Return) {
+                            return Ok(false);
+                        }
 
-                report_builder.add_label(
-                    Label::new(ReportSpan::new(terminator.span))
-                        .with_message(format!(
-                            "Function returns in here without {} moving its value",
+                        report_builder.set_message("Memory leakage after return");
+
+                        report_builder.add_label(
+                            Label::new(ReportSpan::new(terminator.span))
+                                .with_message(format!(
+                                    "Function returns in here without {} moving its value",
+                                    format!("`{}`", linear_local.name).fg(DIAGNOSIS_REPORT_COLOR)
+                                ))
+                                .with_color(DIAGNOSIS_REPORT_COLOR),
+                        );
+                    }
+                    None => {
+                        report_builder.set_message("Memory leakage after return");
+
+                        report_builder.add_note(format!(
+                            "Function ends without {} moving its value",
                             format!("`{}`", linear_local.name).fg(DIAGNOSIS_REPORT_COLOR)
-                        ))
-                        .with_color(DIAGNOSIS_REPORT_COLOR),
-                );
-            }
-            None => {
-                report_builder.set_message("Memory leakage after return");
+                        ));
+                    }
+                }
 
-                report_builder.add_note(format!(
-                    "Function returns without {} moving its value",
+                report_builder.add_help(format!(
+                    "Try to move {}'s value before reaching the return",
                     format!("`{}`", linear_local.name).fg(DIAGNOSIS_REPORT_COLOR)
                 ));
+
+                Ok(true)
             }
+            LinearStatus::Free => {
+                if linear_local.local != RETURN_LOCAL {
+                    return Ok(false);
+                }
+
+                match terminator {
+                    Some(terminator) => {
+                        if !matches!(&terminator.kind, mir::TerminatorKind::Return) {
+                            return Ok(false);
+                        }
+
+                        report_builder.set_message("Ignored linear return type");
+
+                        report_builder.add_label(
+                            Label::new(ReportSpan::new(terminator.span))
+                                .with_message(
+                                    "Function returns in here without returning a linear variable",
+                                )
+                                .with_color(DIAGNOSIS_REPORT_COLOR),
+                        );
+                    }
+                    None => {
+                        report_builder.set_message("Ignored linear return type");
+
+                        report_builder
+                            .add_note("Function ends without returning a linear variable");
+                    }
+                }
+
+                report_builder.add_help("Try to return a linear variable");
+
+                Ok(true)
+            }
+            LinearStatus::Unknown => Ok(false),
         }
-
-        report_builder.add_help(format!(
-            "Try to move {}'s value before reaching the return",
-            format!("`{}`", linear_local.name).fg(DIAGNOSIS_REPORT_COLOR)
-        ));
-
-        Ok(true)
     }
 }
